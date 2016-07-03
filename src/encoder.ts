9493f2f928e843c5137b7896686f925831edf9f0
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-07-03 17:25:35 +0200

    encoder: Convert Encoder to class

--- src/encoder.ts
+++ src/encoder.ts
@@ -1,15 +1,17 @@
-export default function Encoder(buf) {
-  this.buf = buf;
-}
+export default class Encoder {
+  buf;
 
-Encoder.prototype = {
+  constructor(buf) {
+    this.buf = buf;
+  }
+
   separator() {
     this.buf.separator();
-  },
+  }
 
   newline() {
     this.buf.newline();
-  },
+  }
 
   write5(column, source, sourceLine, sourceColumn, name) {
     this.buf.write(column);
@@ -17,20 +19,20 @@ Encoder.prototype = {
     this.buf.write(sourceLine);
     this.buf.write(sourceColumn);
     this.buf.write(name);
-  },
+  }
 
   write4(column, source, sourceLine, sourceColumn) {
     this.buf.write(column);
     this.buf.write(source);
     this.buf.write(sourceLine);
     this.buf.write(sourceColumn);
-  },
+  }
 
   write1(column) {
     this.buf.write(column);
-  },
+  }
 
   get length() {
     return this.buf.length;
-  },
+  }
 };
