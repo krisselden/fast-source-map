5c0bbed9c137f413645b6c1e5d2883fbfd3ce270
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-07-03 17:10:58 +0200

    int-buffer-reader: Convert IntBufferReader to class

--- src/int-buffer-reader.ts
+++ src/int-buffer-reader.ts
@@ -1,21 +1,25 @@
-export default function IntBufferReader(buf, ptr, len) {
-  this.buf = buf;
-  this.ptr = ptr|0;
-  this.limit = (ptr + len)|0;
-}
+export default class IntBufferReader {
+  buf;
+  ptr;
+  limit;
 
-IntBufferReader.prototype = {
+  constructor(buf, ptr, len) {
+    this.buf = buf;
+    this.ptr = ptr|0;
+    this.limit = (ptr + len)|0;
+  }
+
   peek() {
     return this.buf[this.ptr|0]|0;
-  },
+  }
 
   read() {
     var n = this.buf[this.ptr|0]|0;
     this.ptr = (this.ptr + 1)|0;
     return n;
-  },
+  }
 
   next() {
     this.ptr = (this.ptr + 1)|0;
-  },
-};
+  }
+}
