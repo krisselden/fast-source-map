9bcb824481e3c739e41bd606b0d3073d44deab40
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-07-05 15:08:07 +0200

    concat: Extract mapping factory functions

--- src/decoder.ts
+++ src/decoder.ts
@@ -1,5 +1,6 @@
 import { Delegate } from "./mappings-decoder";
 import { FullDecodedMapping, DecodedMappings } from "./interfaces";
+import { createMapping1, createMapping4, createMapping5 } from "./mapping-factories";
 
 export default class Decoder implements Delegate {
   currentLine: Array<FullDecodedMapping> = [];
@@ -12,35 +13,14 @@ export default class Decoder implements Delegate {
   }
 
   mapping1(col) {
-    this.currentLine.push({
-      fieldCount: 1,
-      col,
-      src: 0,
-      srcLine: 0,
-      srcCol: 0,
-      name: 0,
-    });
+    this.currentLine.push(createMapping1(col));
   }
 
   mapping4(col, src, srcLine, srcCol) {
-    this.currentLine.push({
-      fieldCount: 4,
-      col,
-      src,
-      srcLine,
-      srcCol,
-      name: 0,
-    });
+    this.currentLine.push(createMapping4(col, src, srcLine, srcCol));
   }
 
   mapping5(col, src, srcLine, srcCol, name) {
-    this.currentLine.push({
-      fieldCount: 5,
-      col,
-      src,
-      srcLine,
-      srcCol,
-      name,
-    });
+    this.currentLine.push(createMapping5(col, src, srcLine, srcCol, name));
   }
 };
