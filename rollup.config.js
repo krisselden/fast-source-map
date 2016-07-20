09a740ad24b428a16ed951eba654f591d7458f27
Kris Selden <kris.selden@gmail.com>
2016-07-19 17:16:40 -0700

    add browser dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,16 @@
 export default {
   entry: 'lib/index.js',
-  dest: 'dist/index.js',
-  sourceMap: 'dist/index.js.map',
-  format: 'cjs'
+  targets: [
+    {
+      dest: 'dist/index.js',
+      sourceMap: 'dist/index.js.map',
+      format: 'cjs'
+    },
+    {
+      dest: 'dist/browser.js',
+      sourceMap: 'dist/browser.js.map',
+      format: 'iife',
+      moduleName: 'FSM'
+    }
+  ]
 }
